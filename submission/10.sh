5648fb1d8b3361d97322e524b84db30c8900e5e3
mwihoti <danielmwihoti@gmail.com>
2026-06-26 12:31:27 +0300

    add comment to solutions

--- submission/10.sh
+++ submission/10.sh
@@ -1,1 +1,2 @@
-bitcoin-cli -regtest validateaddress "bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy" | jq -r '.isvalid' 
+# Generate a Taproot address.
+bitcoin-cli -regtest validateaddress "bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy" | jq -r '.isvalid'
