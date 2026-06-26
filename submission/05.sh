4569d898a5286417ff8b7c8ada8f425868885b37
mwihoti <danielmwihoti@gmail.com>
2026-06-26 07:19:22 +0300

    fix 05: mine blocks before balance check

--- submission/05.sh
+++ submission/05.sh
@@ -1,1 +1,3 @@
+ADDR=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
+bitcoin-cli -regtest generatetoaddress "$ADDR" 101 > /dev/null
 bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
